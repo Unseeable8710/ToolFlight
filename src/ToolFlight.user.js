58261ee4d333b04318a467fd9ed67fd7b48c7bab
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-06-18 21:35:04 -0400

    Created the source and archive folders, along with the main file

---
+++ src/ToolFlight.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         ToolFlight
+// @namespace    Unseeable's Noteflight Tools
+// @version      1.0.0-b1r0
+// @description  A set of tools and features for the online notation platform called Noteflight.
+// @author       Colton Stone
+// @license      GPL-3.0-or-later
+// @tag          productivity
+// @tag          utilities
+// @match        https://www.noteflight.com/*
+// @icon         https://www.google.com/s2/favicons?sz=32&domain=noteflight.com
+// @grant        unsafeWindow
+// @grant        GM
+// ==/UserScript==
+
+(function() {
+  
+})();
